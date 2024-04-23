cb6824ef15dd9bd4de7d48ed5588871129ff0e5b
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-23 22:06:05 +0400

    Update CommonArr.java

--- CommonArr.java
+++ CommonArr.java
@@ -1,19 +1,34 @@
 public class CommonArr {
 
-    public static void main(String[] args) {
- 
-        int[] arr1 = {1, 2, 5, 6, 5, 7, 10};
-        int[] arr2 = {10, 0, 6, 2, 8, 4, 7};
-        
+  public static void main(String[] args) {
 
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                 if (arr1[i] == (arr2[j])) {
-                    System.out.println("Common element : " + (arr1[i]));
-                }
-               
-            }
+    int[] arr1 = {
+      1,
+      2,
+      5,
+      6,
+      5,
+      7,
+      10
+    };
+    int[] arr2 = {
+      10,
+      0,
+      6,
+      2,
+      8,
+      4,
+      7
+    };
+
+    for (int i = 0; i < arr1.length; i++) {
+      for (int j = 0; j < arr2.length; j++) {
+        if (arr1[i] == (arr2[j])) {
+          System.out.println("Common element : " + (arr1[i]));
         }
-     
+
+      }
     }
+
+  }
 }
